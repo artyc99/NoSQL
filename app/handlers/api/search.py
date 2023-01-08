2673606c1ddbe828f13bece3f4ed17138d2f8d3f
artyc99 <artyc99@mail.ru>
2023-01-08 20:52:16 +0300

    add hazel to search

--- app/handlers/api/search.py
+++ app/handlers/api/search.py
@@ -2,10 +2,18 @@ from flask import Blueprint, Response, request
 
 from ..responses import ApiResponse
 from ...elastic.library import get_book
+from ...hazlecast import map
 
 search = Blueprint('search', __name__, url_prefix='/search')
 
 
 @search.route('', methods=['POST'])
 def get_all() -> Response:
-    return ApiResponse.response200({'res': get_book(request.json['description'])})
+    result = {}
+    hz = map.get(request.json['description'])
+    if hz:
+        result = hz
+    else:
+        result = {'res': get_book(request.json['description'])}
+        map.put(request.json['description'], result)
+    return ApiResponse.response200(result)
